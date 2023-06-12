fcb232bef81d2b7d0becc0c63712355c1f6c26e1
unknown <ferreiravincent115@gmail.com>
2023-06-12 22:21:37 +0200

    3

--- migrations/Version20230612202102.php
+++ migrations/Version20230612202102.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230612174257 extends AbstractMigration
+final class Version20230612202102 extends AbstractMigration
 {
     public function getDescription(): string
     {
